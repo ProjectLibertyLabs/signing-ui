ae47f4b64f4e81f8c711438b43d25efe4bf2965f
shannonwells <shannonwells@users.noreply.github.com>
2023-05-03 16:15:16 -0700

    connected to polkadot API and polkadot{.js} extension

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # signing-ui
-signer-ui
+
+A UI to demonstrate and help with sending signed payloads to a Frequency parachain.
+
+## References 
+https://libertydsnp.github.io/frequency/
+
+## To run locally:
+Start a webserver, such as [Simple Web Server](https://simplewebserver.org/).
+
+Load at http://localhost:8080
+
+## Deployment
+https://libertydsnp.github.io/balance-ui/
---
+++ index.js
@@ -0,0 +1,83 @@
+import { WsProvider, ApiPromise } from 'https://cdn.jsdelivr.net/npm/@polkadot/api@10.5.1/+esm';
+import { web3Accounts, web3Enable, web3FromAddress } from 'https://cdn.jsdelivr.net/npm/@polkadot/extension-dapp@0.46.2/+esm';
+
+
+let PREFIX = 42;
+let UNIT = "UNIT";
+
+let singletonApi;
+let singletonProvider;
+let providerName;
+
+const GENESIS_HASHES = {
+    rococo: "0x0c33dfffa907de5683ae21cc6b4af899b5c4de83f3794ed75b2dc74e1b088e72",
+    frequency: "0x4a587bf17a404e3572747add7aab7bbe56e805a5479c6c436f07f36fcc8d3ae1",
+}
+
+async function loadApi(providerUri) {
+    // Singleton
+    if (!providerUri && singletonApi) return singletonApi;
+    if (!providerUri) {
+        // they didn't select anything
+        return null;
+    }
+    // Handle disconnects
+    if (providerUri) {
+
+        if (singletonApi) {
+            await singletonApi.disconnect();
+        } else if (singletonProvider) {
+            await singletonProvider.disconnect();
+        }
+    }
+
+    // Singleton Provider because it starts trying to connect here.
+    singletonProvider = new WsProvider(providerUri);
+    singletonApi = await ApiPromise.create({provider: singletonProvider});
+
+    await singletonApi.isReady;
+    const chain = await singletonApi.rpc.system.properties();
+    PREFIX = Number(chain.ss58Format.toString());
+    UNIT = chain.tokenSymbol.toHuman();
+    document.getElementById("unit").innerText = UNIT;
+    return singletonApi;
+}
+
+// Connect to the wallet and blockchain
+async function connect(event) {
+    event.preventDefault();
+    let selectedProvider = document.getElementById("provider-list").selectedOptions[0];
+    console.log({selectedProvider});
+    providerName = selectedProvider.getAttribute("name");
+    let api = await loadApi(selectedProvider.getAttribute("value"));
+    console.log({api});
+    await loadAccounts();
+
+}
+
+async function loadAccounts() {
+    // meta.source contains the name of the extension that provides this account
+    const extensions = await web3Enable('Frequency parachain signer helper');
+    if (!extensions.length) {
+        alert("Polkadot{.js} extension not found; please install it first.");
+        return;
+    }
+    let accounts = await web3Accounts();
+    document.getElementById("signing-address").innerHTML = "";
+    accounts.forEach(a => {
+        if (!a.meta.genesisHash || GENESIS_HASHES[providerName] === a.meta.genesisHash) {
+            let el = document.createElement("option");
+            el.setAttribute("value", a.address);
+            el.setAttribute("name", a.address);
+            el.innerText = `${a.meta.name}: ${a.address}`;
+            document.getElementById("signing-address").add(el);
+        }
+    })
+    console.log({accounts})
+}
+
+function init() {
+    document.getElementById("connectButton").addEventListener("click", connect);
+}
+
+init();
